aed52f8db65d4aa2284c9963619086cbed3b99c8
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-06-19 18:50:07 +0530

    mongodb

---
+++ mongodb.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+#checking the user has root access or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R Error: you must have root access to execute the script $N " | tee -a $LOG_FILE
+    exit 1
+else 
+    echo "you are root user.you have root access" | tee -a $LOG_FILE
+fi 
+
+VALIDATE() {
+
+if [ $1 -ne 0 ]
+    then 
+        echo -e "$2....$R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2....$G SUCCESS $N" | tee -a $LOG_FILE
+    fi         
+}
+
+cp mongodb.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "copying mongodb repo"
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "Installing mongodb server"
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "enabling mongodb"
+
+systemctl start mongod &>>$LOG_FILE
+VALIDATE $? "starting mongodb"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf 
+VALIDATE $? "Editing mongodb conf file for remote connections" 
+
+systemctl restart mongod &>>$LOG_FILE
+VALIDATE $? "restarting mongodb" 
+
+
+
+
+
