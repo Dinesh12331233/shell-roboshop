a1661bc81b2b57d919255e77e227fd20be84fd4a
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-06-20 13:16:28 +0530

    shipping

--- shipping.sh
+++ shipping.sh
@@ -80,19 +80,18 @@ VALIDATE $? "starting shipping"
 dnf install mysql -y &>>$LOG_FILE
 VALIDATE $? "installing mysql" 
 
-mysql -h mysql.deepthi.tech -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
-mysql -h mysql.deepthi.tech -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql &>>$LOG_FILE
-mysql -h mysql.deepthi.tech -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
-VALIDATE $? "loading data into MYSQL" 
+mysql -h mysql.deepthi.tech -u root -proboshop@1 -e 'use cities'
+if [ $? -ne 0 ] 
+then
+    mysql -h mysql.deepthi.tech -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
+    mysql -h mysql.deepthi.tech -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql &>>$LOG_FILE
+    mysql -h mysql.deepthi.tech -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
+    VALIDATE $? "loading data into MYSQL"
+else 
+    echo -e "Data is already loaded into MYSQL... $Y skipping $N "
 
 systemctl restart shipping &>>$LOG_FILE
 VALIDATE $? "restarting shipping"
-
-
-
-
-
-
 
 END_TIME=$(date +%s) 
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
