9ecc8f17d8e7f5c0292f9730450a2f7f123ea9b5
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-06-20 10:15:56 +0530

    redis

---
+++ redis.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+#checking the user has root access or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R Error: you must have root access to execute the script $N " | tee -a $LOG_FILE
+    exit 1
+else 
+    echo "you are root user.you have root access" | tee -a $LOG_FILE
+fi 
+
+VALIDATE() {
+
+if [ $1 -ne 0 ]
+    then 
+        echo -e "$2....$R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2....$G SUCCESS $N" | tee -a $LOG_FILE
+    fi         
+}
+
+dnf module disable redis -y
+VALIDATE $? "disabling default version of redis"
+
+dnf module enable redis:7 -y 
+VALIDATE $? "enabling redis:7 version of redis"
+
+dnf install redis -y 
+VALIDATE $? "installing redis"
+
